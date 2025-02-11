d0202542cbfb71043048cc5f7bea826a38adaffd
Isuruni-Weerasingha <isurunidilhara04@gmail.com>
2025-02-12 03:56:19 +0530

    create Feedback interface(2211-0267)

--- app/src/main/java/com/example/upwork/feedback1.java
+++ app/src/main/java/com/example/upwork/feedback1.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.upwork.R;
 
 public class feedback1 extends AppCompatActivity { // Renamed to follow Java conventions
 
